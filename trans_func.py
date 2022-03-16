7cff053f95823739c9d5755f2c9cde314356ce5e
AR <46914247+EKiritsugu@users.noreply.github.com>
2022-03-17 00:04:42 +0800

    老子不干了，睡觉。

--- trans_func.py
+++ trans_func.py
@@ -16,3 +16,16 @@ def Mstft(Sig_ori , fft_len=2048 , lap_len=1024):
         Sw[:,:,i] = Zxx
 
     return Sw
+
+def Mistft(Sw , fft_len= 2048  ,lap_len = 1024):
+    # Sw : n_freq n_frame n_ch
+    # return: nch, t
+    K = Sw.shape[2]
+    ## istft
+    _ , tmp = signal.istft(Y[:,:,0], nperseg=fft_len , noverlap=lap_len)
+    St_hat = np.zeros((K , len(tmp)))
+    St_hat[0,:] = np.real(tmp)
+    for i in range(K):
+        _ , tmp = signal.istft(Y[:,:,i], nperseg=fft_len , noverlap=lap_len)
+        St_hat[i,:] = np.real(tmp)
+    return St_hat
