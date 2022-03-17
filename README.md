23b8608188ba16b6d39943e387dbd7dacd121bfd
AR <46914247+EKiritsugu@users.noreply.github.com>
2022-03-17 11:06:47 +0800

    adjust

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # audio_toolbox
  
+关于这份toolbox的规范：
+
+## input & output
+
++ 多通道时域信号一律按照 t , n_channel格式进行输入和输出
+
++ 多通道频域信号一律按照 n_freq , n_frame , n_channel格式进行输入输出
+
+
+## function
+
++ 除了待处理的信号外，所有的参数一律需要有缺省模式，尤其是缺省模式
--- trans_func.py
+++ trans_func.py
@@ -2,30 +2,32 @@ from scipy import signal
 
 
 def Mstft(Sig_ori , fft_len=2048 , lap_len=1024):
-    # Sig_ori: nch, t
+    # Sig_ori: t , nch
     # return : n_freq n_frame n_ch
-    [M,_] = np.shape(Sig_ori)
+    [_,M] = np.shape(Sig_ori)
 
     ##stft
-    _,_,Zxx0 = signal.stft(Sig_ori[0,:] , nperseg=fft_len , noverlap=lap_len)
+    _,_,Zxx0 = signal.stft(Sig_ori[:,0] , nperseg=fft_len , noverlap=lap_len)
     a,b = np.shape(Zxx0)
     Sw = np.zeros((a,b,M) , dtype=complex)
     Sw[:,:,0] = Zxx0
     for i in range(1,M):
-        f_list,_,Zxx = signal.stft(Sig_ori[i,:] , nperseg=fft_len , noverlap=lap_len)
+        f_list,_,Zxx = signal.stft(Sig_ori[:,i] , nperseg=fft_len , noverlap=lap_len)
         Sw[:,:,i] = Zxx
 
     return Sw
 
 def Mistft(Sw , fft_len= 2048  ,lap_len = 1024):
     # Sw : n_freq n_frame n_ch
-    # return: nch, t
+    # return: t , nch
     K = Sw.shape[2]
     ## istft
     _ , tmp = signal.istft(Y[:,:,0], nperseg=fft_len , noverlap=lap_len)
-    St_hat = np.zeros((K , len(tmp)))
+    St_hat = np.zeros((len(tmp) , K))
     St_hat[0,:] = np.real(tmp)
     for i in range(K):
         _ , tmp = signal.istft(Y[:,:,i], nperseg=fft_len , noverlap=lap_len)
-        St_hat[i,:] = np.real(tmp)
+        St_hat[:,K] = np.real(tmp)
     return St_hat
+
+    
